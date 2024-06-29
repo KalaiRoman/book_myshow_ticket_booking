d48e0a74746c9a4ff183d047ade8f15927c6ef22
kalai <kalai@cdp360.com>
2024-06-29 21:41:38 +0530

    ticket slecting login implemented

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -11,29 +11,59 @@ function Home() {
   const dispatch=useDispatch();
   const state=useSelector((state)=>state?.tickBook);
   const [YourBookedticket,setYourBookedTicket]=useState([]);
+
+  const [error,setError]=useState("");
   const {Tickets}=state;
+
   useEffect(()=>{
   var AllInputSheats=[];
 for(let i=1;i<BookingSheats?.length;i++)
   {
     const values={
       index:`S${i}`,
+      status:false
     }
     AllInputSheats.push(values);
   }
   setSheats(AllInputSheats);
   },[]);
 
+  const TicketBooked=(paramsTicketId)=>{
+
+      setError("One Person Booked Ticket Only 10, Please Login Another Enmail Id");
+      if (paramsTicketId) {
+        if (Tickets?.includes(paramsTicketId)) {
+
+          const filterLocalTickets=YourBookedticket?.filter((item)=>item!==paramsTicketId);
+          setYourBookedTicket(filterLocalTickets)
+            dispatch(filterTicket(paramsTicketId));
+        }
+        else {
+          if(YourBookedticket?.length>9)
+            {
+
+            }
+            else{
+              setYourBookedTicket([...YourBookedticket, paramsTicketId]);
+              dispatch(BookTicket(paramsTicketId));
+            }
+         
+      }
+      }
+  }
+
   const handleTicketBooking = useCallback((paramsTicketId) => {
     if (paramsTicketId) {
         if (Tickets?.includes(paramsTicketId)) {
+          const filterLocalTickets=YourBookedticket?.filter((item)=>item!==paramsTicketId);
+          setYourBookedTicket(filterLocalTickets)
             dispatch(filterTicket(paramsTicketId));
         } else {
             setYourBookedTicket([...YourBookedticket, paramsTicketId]);
             dispatch(BookTicket(paramsTicketId));
         }
     }
-}, [Tickets]);
+}, [Tickets,error]);
 
   return (
     <div>
@@ -42,12 +72,19 @@ for(let i=1;i<BookingSheats?.length;i++)
      </section>
       <section>
       <div className='w-[100%] h-[100vh] overflow-hidden lg:h-[100%]'>
+      <div className='text-danger'>
+        {YourBookedticket?.length>9 && <>
+        
+    {error && <div>{error}</div>}
+    </>}
+  </div>
 <div className='d-flex gap-2 w-[100%] h-[100%]'>
+
 <div className='col-lg-6 w-[50%]'>
-<LeftSheats BookingSheats={sheat} handleTicketBooking={handleTicketBooking} Tickets={Tickets}/>
+<LeftSheats BookingSheats={sheat} handleTicketBooking={YourBookedticket?.length>9?TicketBooked:handleTicketBooking} Tickets={Tickets}/>
 </div>
 <div className='col-lg-6 w-[50%]'>
-  <RightSheats BookingSheats={sheat}/>
+  <RightSheats BookingSheats={sheat} handleTicketBooking={YourBookedticket?.length>9?TicketBooked:handleTicketBooking} Tickets={Tickets}/>
 </div>
 </div>
       </div>
--- client/src/components/home/RightSheats.js
+++ client/src/components/home/RightSheats.js
@@ -1,12 +1,12 @@
 import React,{memo} from 'react'
-function RightSheats({BookingSheats}) {
+function RightSheats({BookingSheats,Tickets,handleTicketBooking}) {
   return (
     <div className='p-[5%]'>
         <div className='row gap-2'>
 {BookingSheats?.slice(170,340)?.map((item,index)=>{
     return(
-        <div className='sheat-box col-lg-1' key={index+2}>
-         {item?.index}
+        <div className={`${Tickets?.includes(item?.index)?"booked-box col-lg-1":"sheat-box col-lg-1"}`} key={index} onClick={()=>handleTicketBooking(item?.index)}>
+           {item?.index}
         </div>
     )
 })}
